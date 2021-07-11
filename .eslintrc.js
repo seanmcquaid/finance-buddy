fbc81dc46b27ac3a5a174a1da0bc098b2be7fd28
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-10 20:21:16 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+    'plugin:cypress/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jest/expect-expect': 'off',
+  },
+};
---
+++ src/services/usersService.js
@@ -0,0 +1,10 @@
+import apiClient from './apiClient';
+
+const usersService = {
+  login: (username, password) =>
+    apiClient.post(`${process.env.REACT_APP_API_URL}/users/login`, { username, password }),
+  register: (username, password) =>
+    apiClient.post(`${process.env.REACT_APP_API_URL}/users/register`, { username, password }),
+};
+
+export default usersService;
