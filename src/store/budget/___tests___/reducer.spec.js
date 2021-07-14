835c650a51fe8bd0e2a661e74bdbb6565ede528b
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-14 15:04:11 -0400

    sketched out scenarios for reducer tests

---
+++ src/store/budget/___tests___/reducer.spec.js
@@ -0,0 +1,25 @@
+describe('budget reducer', () => {
+  it('SET_TOTAL_BUDGET', () => {});
+
+  it('CALCULATE_TOTAL_PERCENTAGE', () => {});
+
+  it('CALCULATE_REMAINING_AMOUNT', () => {});
+
+  it('ADD_FIXED_COST_ENTRY', () => {});
+
+  it('UPDATE_FIXED_COST_ENTRY', () => {});
+
+  it('DELETE_FIXED_COST_ENTRY', () => {});
+
+  it('ADD_FLEXIBLE_SPENDING_ENTRY', () => {});
+
+  it('UPDATE_FLEXIBLE_SPENDING_ENTRY', () => {});
+
+  it('DELETE_FLEXIBLE_SPENDING_ENTRY', () => {});
+
+  it('ADD_SAVINGS_ENTRY', () => {});
+
+  it('UPDATE_SAVINGS_ENTRY', () => {});
+
+  it('DELETE_SAVINGS_ENTRY', () => {});
+});
