7ef7e3d6cc26c738949b04b3f89e106f5a64d1e4
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-14 14:45:29 -0400

    starting scenarios for action creators unit tests

---
+++ src/store/budget/___tests___/actionCreators.spec.js
@@ -0,0 +1,25 @@
+describe('budget actionCreators', () => {
+  it('setTotalBudget', () => {});
+
+  it('calculateTotalPercentage', () => {});
+
+  it('calculateRemainingAmount', () => {});
+
+  it('addFixedCostEntry', () => {});
+
+  it('addFlexibleSpendingEntry', () => {});
+
+  it('addSavingsEntry', () => {});
+
+  it('updateFixedCostEntry', () => {});
+
+  it('updateFlexibleSpendingEntry', () => {});
+
+  it('updateSavingsEntry', () => {});
+
+  it('deleteFixedCostEntry', () => {});
+
+  it('deleteFlexibleSpendingEntry', () => {});
+
+  it('deleteSavingsEntry', () => {});
+});
