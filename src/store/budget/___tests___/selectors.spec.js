389ddbe994fac0950025a152618ed3f64f6e5755
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-14 14:26:16 -0400

    starting to work on unit tests for selectors for budget

--- src/store/budget/___tests___/selectors.spec.js
+++ src/store/budget/___tests___/selectors.spec.js
@@ -1,25 +1,80 @@
-import { fixedCostsSelector } from '../selectors';
+import {
+  budgetSelector,
+  fixedCostsPercentageSelector,
+  fixedCostsSelector,
+} from '../selectors';
 
 describe('budget selectors', () => {
-  it('fixedCostsSelector', () => {
-    const initialState = {
-      budget: {
-        fixedCostsPercentage: 0,
-        fixedCosts: {
-          rent: 800,
-        },
-        fixedCostsTotal: 0,
-        flexibleSpendingPercentage: 0,
-        flexibleSpending: {},
-        flexibleSpendingTotal: 0,
-        savingsPercentage: 0,
-        savings: {},
-        savingsTotal: 0,
-        totalBudget: 0,
-        remainingAmount: 0,
-        totalPercentage: 100,
+  const state = {
+    budget: {
+      fixedCostsPercentage: 0,
+      fixedCosts: {
+        rent: 800,
       },
-    };
-    expect(fixedCostsSelector(initialState)).toEqual([{ rent: 800 }]);
+      fixedCostsTotal: 0,
+      flexibleSpendingPercentage: 0,
+      flexibleSpending: {
+        cards: 200,
+      },
+      flexibleSpendingTotal: 0,
+      savingsPercentage: 0,
+      savings: {
+        chase: 2000,
+      },
+      savingsTotal: 0,
+      totalBudget: 0,
+      remainingAmount: 0,
+      totalPercentage: 100,
+    },
+  };
+  it('budgetSelector', () => {
+    expect(budgetSelector(state)).toEqual({
+      fixedCostsPercentage: 0,
+      fixedCosts: {
+        rent: 800,
+      },
+      fixedCostsTotal: 0,
+      flexibleSpendingPercentage: 0,
+      flexibleSpending: {
+        cards: 200,
+      },
+      flexibleSpendingTotal: 0,
+      savingsPercentage: 0,
+      savings: {
+        chase: 2000,
+      },
+      savingsTotal: 0,
+      totalBudget: 0,
+      remainingAmount: 0,
+      totalPercentage: 100,
+    });
   });
+
+  it('fixedCostsPercentageSelector', () => {
+    expect(fixedCostsPercentageSelector(state)).toEqual(0);
+  });
+
+  it('fixedCostsSelector', () => {
+    expect(fixedCostsSelector(state)).toEqual([{ rent: 800 }]);
+  });
+
+  it('fixedCostsTotalSelector', () => {});
+
+  it('flexibleSpendingPercentageSelector', () => {});
+
+  it('flexibleSpendingSelector', () => {});
+
+  it('flexibleSpendingTotalSelector', () => {});
+
+  it('savingsPercentageSelector', () => {});
+
+  it('savingsSelector', () => {});
+
+  it('savingsTotalSelector', () => {});
+
+  it('totalBudgetSelector', () => {});
+
+  it('remainingAmountSelector', () => {});
+
+  it('totalPercentageSelector', () => {});
 });
