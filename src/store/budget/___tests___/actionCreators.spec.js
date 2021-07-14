5f06d54baf0a00ba35031ca9a918daaf8956455f
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-14 14:57:27 -0400

    finished action creators tests

--- src/store/budget/___tests___/actionCreators.spec.js
+++ src/store/budget/___tests___/actionCreators.spec.js
@@ -1,25 +1,112 @@
+import {
+  addFixedCostEntry,
+  addFlexibleSpendingEntry,
+  addSavingsEntry,
+  calculateRemainingAmount,
+  calculateTotalPercentage,
+  deleteFixedCostEntry,
+  deleteFlexibleSpendingEntry,
+  deleteSavingsEntry,
+  setTotalBudget,
+  updateFixedCostEntry,
+  updateFlexibleSpendingEntry,
+  updateSavingsEntry,
+} from '../actionCreators';
+import {
+  ADD_FIXED_COST_ENTRY,
+  ADD_FLEXIBLE_SPENDING_ENTRY,
+  ADD_SAVINGS_ENTRY,
+  CALCULATE_REMAINING_AMOUNT,
+  CALCULATE_TOTAL_PERCENTAGE,
+  DELETE_FIXED_COST_ENTRY,
+  DELETE_FLEXIBLE_SPENDING_ENTRY,
+  DELETE_SAVINGS_ENTRY,
+  SET_TOTAL_BUDGET,
+  UPDATE_FIXED_COST_ENTRY,
+  UPDATE_FLEXIBLE_SPENDING_ENTRY,
+  UPDATE_SAVINGS_ENTRY,
+} from '../actionTypes';
+
 describe('budget actionCreators', () => {
-  it('setTotalBudget', () => {});
+  it('setTotalBudget', () => {
+    expect(setTotalBudget(100)).toEqual({
+      type: SET_TOTAL_BUDGET,
+      payload: { total: 100 },
+    });
+  });
 
-  it('calculateTotalPercentage', () => {});
+  it('calculateTotalPercentage', () => {
+    expect(calculateTotalPercentage()).toEqual({
+      type: CALCULATE_TOTAL_PERCENTAGE,
+    });
+  });
 
-  it('calculateRemainingAmount', () => {});
+  it('calculateRemainingAmount', () => {
+    expect(calculateRemainingAmount()).toEqual({
+      type: CALCULATE_REMAINING_AMOUNT,
+    });
+  });
 
-  it('addFixedCostEntry', () => {});
+  it('addFixedCostEntry', () => {
+    expect(addFixedCostEntry('rent')).toEqual({
+      type: ADD_FIXED_COST_ENTRY,
+      payload: { name: 'rent' },
+    });
+  });
 
-  it('addFlexibleSpendingEntry', () => {});
+  it('addFlexibleSpendingEntry', () => {
+    expect(addFlexibleSpendingEntry('food')).toEqual({
+      type: ADD_FLEXIBLE_SPENDING_ENTRY,
+      payload: { name: 'food' },
+    });
+  });
 
-  it('addSavingsEntry', () => {});
+  it('addSavingsEntry', () => {
+    expect(addSavingsEntry('chase')).toEqual({
+      type: ADD_SAVINGS_ENTRY,
+      payload: { name: 'chase' },
+    });
+  });
 
-  it('updateFixedCostEntry', () => {});
+  it('updateFixedCostEntry', () => {
+    expect(updateFixedCostEntry('rent', 1000)).toEqual({
+      type: UPDATE_FIXED_COST_ENTRY,
+      payload: { name: 'rent', amount: 1000 },
+    });
+  });
 
-  it('updateFlexibleSpendingEntry', () => {});
+  it('updateFlexibleSpendingEntry', () => {
+    expect(updateFlexibleSpendingEntry('food', 100)).toEqual({
+      type: UPDATE_FLEXIBLE_SPENDING_ENTRY,
+      payload: { name: 'food', amount: 100 },
+    });
+  });
 
-  it('updateSavingsEntry', () => {});
+  it('updateSavingsEntry', () => {
+    expect(updateSavingsEntry('chase', 2000)).toEqual({
+      type: UPDATE_SAVINGS_ENTRY,
+      payload: { name: 'chase', amount: 2000 },
+    });
+  });
 
-  it('deleteFixedCostEntry', () => {});
+  it('deleteFixedCostEntry', () => {
+    expect(deleteFixedCostEntry('rent')).toEqual({
+      type: DELETE_FIXED_COST_ENTRY,
+      payload: { name: 'rent' },
+    });
+  });
 
-  it('deleteFlexibleSpendingEntry', () => {});
+  it('deleteFlexibleSpendingEntry', () => {
+    expect(deleteFlexibleSpendingEntry('food')).toEqual({
+      type: DELETE_FLEXIBLE_SPENDING_ENTRY,
+      payload: { name: 'food' },
+    });
+  });
 
-  it('deleteSavingsEntry', () => {});
+  it('deleteSavingsEntry', () => {
+    expect(deleteSavingsEntry('chase')).toEqual({
+      type: DELETE_SAVINGS_ENTRY,
+      payload: { name: 'chase' },
+    });
+  });
 });
