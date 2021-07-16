4c863719580884d79135e8aecf303ddba17fff59
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-15 23:58:34 -0400

    sit blocks for each test

--- src/store/budget/___tests___/actions.spec.js
+++ src/store/budget/___tests___/actions.spec.js
@@ -3,4 +3,24 @@
 // dispatch actions to return promise
 // get actions from the mock store to compare against expected values
 
-describe('budget actions', () => {});
+describe('budget actions', () => {
+  it('setTotalBudgetAction', () => {});
+
+  it('addFixedCostEntryAction', () => {});
+
+  it('addFlexibleSpendingEntryAction', () => {});
+
+  it('addSavingsEntryAction', () => {});
+
+  it('updateFixedCostEntryAction', () => {});
+
+  it('updateFlexibleSpendingEntryAction', () => {});
+
+  it('updateSavingsEntryAction', () => {});
+
+  it('deleteFixedCostEntryAction', () => {});
+
+  it('deleteFlexibleSpendingEntryAction', () => {});
+
+  it('deleteSavingsEntryAction', () => {});
+});
