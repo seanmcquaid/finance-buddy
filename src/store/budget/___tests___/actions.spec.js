6122081c8b3e2fe8dd141be2b8cc784c7ad4d33f
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-15 22:44:05 -0400

    todo for actions tests

--- src/store/budget/___tests___/actions.spec.js
+++ src/store/budget/___tests___/actions.spec.js
@@ -1,1 +1,6 @@
+// To do
+// Use mock store with initial state
+// dispatch actions to return promise
+// get actions from the mock store to compare against expected values
+
 describe('budget actions', () => {});
