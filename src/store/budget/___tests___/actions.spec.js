4528fd717444f03d3b9d6ce764f9bfbf7e9674f6
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-16 09:45:40 -0400

    set up design pattern for the rest of the tests

--- src/store/budget/___tests___/actions.spec.js
+++ src/store/budget/___tests___/actions.spec.js
@@ -1,10 +1,36 @@
-// To do
-// Use mock store with initial state
-// dispatch actions to return promise
-// get actions from the mock store to compare against expected values
+import configureMockStore from '../../../testUtils/configureMockStore';
+import { setTotalBudgetAction } from '../actions';
 
 describe('budget actions', () => {
-  it('setTotalBudgetAction', () => {});
+  const state = {
+    budget: {
+      fixedCostsPercentage: 0,
+      fixedCosts: {},
+      fixedCostsTotal: 0,
+      flexibleSpendingPercentage: 0,
+      flexibleSpending: {},
+      flexibleSpendingTotal: 0,
+      savingsPercentage: 0,
+      savings: {},
+      savingsTotal: 0,
+      totalBudget: 0,
+      remainingAmount: 0,
+      totalPercentage: 100,
+    },
+  };
+  const store = configureMockStore(state);
+
+  afterEach(() => {
+    store.clearActions();
+  });
+
+  it('setTotalBudgetAction', () => {
+    store.dispatch(setTotalBudgetAction(1000));
+
+    const result = [{ type: 'SET_TOTAL_BUDGET', payload: { total: 1000 } }];
+
+    expect(result).toEqual(store.getActions());
+  });
 
   it('addFixedCostEntryAction', () => {});
 
