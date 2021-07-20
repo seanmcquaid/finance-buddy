c7e1fc0839ee5de0c84655bee7f106846fe28ee4
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-20 12:00:50 -0400

    added total budget form

---
+++ src/containers/Budget/TotalBudgetForm/index.js
@@ -0,0 +1,56 @@
+import { useFormik } from 'formik';
+import styled from 'styled-components';
+import { Button, TextInput } from '../../../components';
+import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { setTotalBudgetAction } from '../../../store/budget/actions';
+
+const digitsOnly = (value) => /^\d+$/.test(value);
+
+const validationSchema = Yup.object().shape({
+  totalBudget: Yup.string().test(
+    'Digits only',
+    'The field should have digits only',
+    digitsOnly,
+  ),
+});
+
+const TotalBudgetForm = () => {
+  const dispatch = useDispatch();
+  const { handleSubmit, handleChange, values, errors } = useFormik({
+    initialValues: {
+      totalBudget: '0',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+      dispatch(setTotalBudgetAction(Number.parseInt(values.totalBudget)));
+    },
+  });
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      {errors.totalBudget && (
+        <TotalBudgetErrorMessage>{errors.totalBudget}</TotalBudgetErrorMessage>
+      )}
+      <TextInput
+        onChange={handleChange}
+        name="totalBudget"
+        label="Total Budget"
+        type="text"
+        value={values.totalBudget}
+      />
+      <Button
+        type="submit"
+        label="Set Total Budget"
+        disabled={!!errors.totalBudget}
+      />
+    </Form>
+  );
+};
+
+const Form = styled.form``;
+
+const TotalBudgetErrorMessage = styled.span``;
+
+export default TotalBudgetForm;
--- src/containers/Budget/index.js
+++ src/containers/Budget/index.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
+import { H1 } from '../../components';
+import TotalBudgetForm from './TotalBudgetForm';
 import TotalBudgetInfo from './TotalBudgetInfo';
 
 const Budget = () => (
   <PageContainer>
-    <Header>Budget Calculator</Header>
+    <Header>
+      <H1>Budget Calculator</H1>
+    </Header>
     <Main>
       <TotalBudgetInfo />
+      <TotalBudgetForm />
     </Main>
   </PageContainer>
 );
