83b510604a257af2ec21fe701669284b30a9ed84
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-19 22:02:40 -0400

    finished total budget info component

---
+++ src/containers/Budget/TotalBudgetInfo/index.js
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { P } from '../../../components';
+import {
+  remainingAmountSelector,
+  totalBudgetSelector,
+  totalPercentageSelector,
+} from '../../../store/budget/selectors';
+
+const TotalBudgetInfo = () => {
+  const totalBudget = useSelector(totalBudgetSelector);
+  const remainingAmount = useSelector(remainingAmountSelector);
+  const totalPercentage = useSelector(totalPercentageSelector);
+
+  return (
+    <TotalBudgetInfoContainer>
+      <P>Total Budget : {totalBudget}</P>
+      <P>Remaining Amount : {remainingAmount}</P>
+      <P>Percentage Accounted For : {totalPercentage}</P>
+    </TotalBudgetInfoContainer>
+  );
+};
+
+const TotalBudgetInfoContainer = styled.div``;
+
+export default TotalBudgetInfo;
