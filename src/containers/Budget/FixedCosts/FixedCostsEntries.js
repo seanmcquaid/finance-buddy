870caa6549da92fe83ff184db83299141f863449
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-21 18:00:46 -0400

    added entries object, might look at making these component reusable and pass in the selector as props?

--- src/containers/Budget/FixedCosts/FixedCostsEntries.js
+++ src/containers/Budget/FixedCosts/FixedCostsEntries.js
@@ -1,13 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { fixedCostsSelector } from '../../../store/budget/selectors';
+import {
+  fixedCostsAsObjectSelector,
+  fixedCostsSelector,
+} from '../../../store/budget/selectors';
 import { deleteFixedCostEntryAction } from '../../../store/budget/actions';
 
 const FixedCostsEntries = () => {
   const entriesList = useSelector(fixedCostsSelector);
+  const entriesObject = useSelector(fixedCostsAsObjectSelector);
   const dispatch = useDispatch();
 
-  // to do - include delete button action
   // to do - include update input action
 
   const deleteButtonOnClick = (name) => {
