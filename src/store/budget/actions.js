13137cb35201c582a67158ef8f2d2b00a501a1ef
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-24 14:26:03 -0400

    added fix for bug and simplified actions

--- src/store/budget/actions.js
+++ src/store/budget/actions.js
@@ -15,78 +15,60 @@ import {
   calculateCategoryTotals,
 } from './actionCreators';
 
-export const setTotalBudgetAction = (total) => (dispatch) =>
-  dispatch(setTotalBudget(total));
-
-export const addFixedCostEntryAction = (name) => (dispatch) => {
-  dispatch(addFixedCostEntry(name));
+export const calculateTotals = () => (dispatch) => {
   dispatch(calculateCategoryPercentages());
   dispatch(calculateCategoryTotals());
   dispatch(calculateTotalPercentage());
   dispatch(calculateRemainingAmount());
+};
+
+export const setTotalBudgetAction = (total) => (dispatch) => {
+  dispatch(setTotalBudget(total));
+  dispatch(calculateTotals());
+};
+
+export const addFixedCostEntryAction = (name) => (dispatch) => {
+  dispatch(addFixedCostEntry(name));
+  dispatch(calculateTotals());
 };
 
 export const addFlexibleSpendingEntryAction = (name) => (dispatch) => {
   dispatch(addFlexibleSpendingEntry(name));
-  dispatch(calculateCategoryPercentages());
-  dispatch(calculateCategoryTotals());
-  dispatch(calculateTotalPercentage());
-  dispatch(calculateRemainingAmount());
+  dispatch(calculateTotals());
 };
 
 export const addSavingsEntryAction = (name) => (dispatch) => {
   dispatch(addSavingsEntry(name));
-  dispatch(calculateCategoryPercentages());
-  dispatch(calculateCategoryTotals());
-  dispatch(calculateTotalPercentage());
-  dispatch(calculateRemainingAmount());
+  dispatch(calculateTotals());
 };
 
 export const updateFixedCostEntryAction = (name, amount) => (dispatch) => {
   dispatch(updateFixedCostEntry(name, amount));
-  dispatch(calculateCategoryPercentages());
-  dispatch(calculateCategoryTotals());
-  dispatch(calculateTotalPercentage());
-  dispatch(calculateRemainingAmount());
+  dispatch(calculateTotals());
 };
 
 export const updateFlexibleSpendingEntryAction =
   (name, amount) => (dispatch) => {
     dispatch(updateFlexibleSpendingEntry(name, amount));
-    dispatch(calculateCategoryPercentages());
-    dispatch(calculateCategoryTotals());
-    dispatch(calculateTotalPercentage());
-    dispatch(calculateRemainingAmount());
+    dispatch(calculateTotals());
   };
 
 export const updateSavingsEntryAction = (name, amount) => (dispatch) => {
   dispatch(updateSavingsEntry(name, amount));
-  dispatch(calculateCategoryPercentages());
-  dispatch(calculateCategoryTotals());
-  dispatch(calculateTotalPercentage());
-  dispatch(calculateRemainingAmount());
+  dispatch(calculateTotals());
 };
 
 export const deleteFixedCostEntryAction = (name) => (dispatch) => {
   dispatch(deleteFixedCostEntry(name));
-  dispatch(calculateCategoryPercentages());
-  dispatch(calculateCategoryTotals());
-  dispatch(calculateTotalPercentage());
-  dispatch(calculateRemainingAmount());
+  dispatch(calculateTotals());
 };
 
 export const deleteFlexibleSpendingEntryAction = (name) => (dispatch) => {
   dispatch(deleteFlexibleSpendingEntry(name));
-  dispatch(calculateCategoryPercentages());
-  dispatch(calculateCategoryTotals());
-  dispatch(calculateTotalPercentage());
-  dispatch(calculateRemainingAmount());
+  dispatch(calculateTotals());
 };
 
 export const deleteSavingsEntryAction = (name) => (dispatch) => {
   dispatch(deleteSavingsEntry(name));
-  dispatch(calculateCategoryPercentages());
-  dispatch(calculateCategoryTotals());
-  dispatch(calculateTotalPercentage());
-  dispatch(calculateRemainingAmount());
+  dispatch(calculateTotals());
 };
