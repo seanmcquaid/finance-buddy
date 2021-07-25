7928510e47ce97989710e4b4df3b878b325367ab
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-25 16:38:58 -0400

    wrote out scenarios for unit testing entries

---
+++ src/containers/Budget/Entries/___tests___/Entries.spec.js
@@ -0,0 +1,7 @@
+describe('<Entries/>', () => {
+  describe('onChange', () => {
+    it('event.target.value contains only numbers', () => {});
+
+    it('event.target.value contains a non number', () => {});
+  });
+});
