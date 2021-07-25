e499f85d772a54a4f034216c8d1db4ee3937efaf
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-25 16:51:14 -0400

    deleted unused files now

--- src/containers/Budget/FlexibleSpending/index.js
+++ src/containers/Budget/FlexibleSpending/index.js
@@ -1,12 +1,30 @@
 import styled from 'styled-components';
 import AddFlexibleSpendingForm from './AddFlexibleSpendingForm';
-import FlexibleSpendingEntries from './FlexibleSpendingEntries';
-import FlexibleSpendingTotals from './FlexibleSpendingTotals';
+import Entries from './Entries';
+import Totals from './Totals';
+import {
+  flexibleSpendingEntriesAsObjectSelector,
+  flexibleSpendingEntriesSelector,
+  flexibleSpendingPercentageSelector,
+  flexibleSpendingTotalSelector,
+} from '../../../store/budget/selectors';
+import {
+  deleteFlexibleSpendingEntryAction,
+  updateFlexibleSpendingEntryAction,
+} from '../../../store/budget/actions';
 
 const FlexibleSpending = () => (
   <FlexibleSpendingContainer>
-    <FlexibleSpendingTotals />
-    <FlexibleSpendingEntries />
+    <Totals
+      totalSelector={flexibleSpendingTotalSelector}
+      percentageSelector={flexibleSpendingPercentageSelector}
+    />
+    <Entries
+      entriesSelector={flexibleSpendingEntriesSelector}
+      entriesAsObjectSelector={flexibleSpendingEntriesAsObjectSelector}
+      updateEntryAction={updateFlexibleSpendingEntryAction}
+      deleteEntryAction={deleteFlexibleSpendingEntryAction}
+    />
     <AddFlexibleSpendingForm />
   </FlexibleSpendingContainer>
 );
--- src/containers/Budget/Savings/index.js
+++ src/containers/Budget/Savings/index.js
@@ -1,12 +1,30 @@
 import styled from 'styled-components';
 import AddSavingsForm from './AddSavingsForm';
-import SavingsEntries from './SavingsEntries';
-import SavingsTotals from './SavingsTotals';
+import Entries from './Entries';
+import Totals from './Totals';
+import {
+  savingsEntriesAsObjectSelector,
+  savingsEntriesSelector,
+  savingsPercentageSelector,
+  savingsTotalSelector,
+} from '../../../store/budget/selectors';
+import {
+  deleteSavingsEntryAction,
+  updateSavingsEntryAction,
+} from '../../../store/budget/actions';
 
 const Savings = () => (
   <SavingsContainer>
-    <SavingsTotals />
-    <SavingsEntries />
+    <Totals
+      totalSelector={savingsTotalSelector}
+      percentageSelector={savingsPercentageSelector}
+    />
+    <Entries
+      entriesSelector={savingsEntriesSelector}
+      entriesAsObjectSelector={savingsEntriesAsObjectSelector}
+      updateEntryAction={updateSavingsEntryAction}
+      deleteEntryAction={deleteSavingsEntryAction}
+    />
     <AddSavingsForm />
   </SavingsContainer>
 );
