037cae8c093c01ba24240363e8f74050004ed669
seanmcquaid <sean.mcquaid2@verizon.net>
2021-08-01 15:46:08 -0400

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,44 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import constants from '../constants';
 
 const Navbar = () => (
   <StyledNav>
     <NavLinks>
       <NavLinkListItem>
         <NavLink to="/">Home</NavLink>
+      </NavLinkListItem>
+      <NavLinkListItem>
         <NavLink to="/budget">Budget</NavLink>
+      </NavLinkListItem>
+      <NavLinkListItem>
         <NavLink to="/mortgage">Mortgage</NavLink>
+      </NavLinkListItem>
+      <NavLinkListItem>
         <NavLink to="/retirement">Retirement</NavLink>
       </NavLinkListItem>
     </NavLinks>
   </StyledNav>
 );
 
-const StyledNav = styled.nav``;
+const StyledNav = styled.nav`
+  width: 100%;
+  background-color: ${constants.darkBlue};
+  padding: 1rem;
+`;
 
-const NavLinks = styled.ul``;
+const NavLinks = styled.ul`
+  list-style: none;
+  display: flex;
+`;
 
-const NavLinkListItem = styled.li``;
+const NavLinkListItem = styled.li`
+  padding: 0 0.5rem;
+`;
 
-const NavLink = styled(Link)``;
+const NavLink = styled(Link)`
+  color: ${constants.white};
+  text-decoration: none;
+`;
 
 export default Navbar;
