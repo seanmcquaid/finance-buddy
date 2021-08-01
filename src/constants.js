fc2ab19cf5c67b038509e77eb19bf24baf33178b
seanmcquaid <sean.mcquaid2@verizon.net>
2021-07-31 22:48:43 -0400

    added color palette choice tto constants

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,12 @@
 const constants = {
-  backgroundColor: '',
-  foregroundColor: '',
-  textColor: '',
+  backgroundColor: '#A4B0F5',
+  foregroundColor: '#4464AD',
+  textColorForBackground: 'black',
+  textColorForForeground: 'white',
   headerFont: "'Nunito', sans-serif",
   paragraphFont: "'Nunito', sans-serif",
 };
 
 export default constants;
+
+// consider 466995 as a color choice as well for foreground
