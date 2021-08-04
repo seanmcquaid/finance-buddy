02469507aaf87089453017a50fd59cf62c9a9541
seanmcquaid <sean.mcquaid2@verizon.net>
2021-08-03 21:35:07 -0400

    finished actions

--- src/store/mortgage/actions.js
+++ src/store/mortgage/actions.js
@@ -1,6 +1,27 @@
-import { setDownPayment, calculateMonthlyPayment } from './actionCreators';
+import {
+  setDownPayment,
+  calculateMonthlyPayment,
+  setTotalMortgage,
+  setInterestRate,
+  setMortgageLength,
+} from './actionCreators';
 
 export const setDownPaymentAction = (downPayment) => (dispatch) => {
   dispatch(setDownPayment(downPayment));
+  dispatch(calculateMonthlyPayment());
+};
+
+export const setTotalMortgageAction = (totalMortgage) => (dispatch) => {
+  dispatch(setTotalMortgage(totalMortgage));
+  dispatch(calculateMonthlyPayment());
+};
+
+export const setInterestRateAction = (interestRate) => (dispatch) => {
+  dispatch(setInterestRate(interestRate));
+  dispatch(calculateMonthlyPayment());
+};
+
+export const setMortgageLengthAction = (mortgageLength) => (dispatch) => {
+  dispatch(setMortgageLength(mortgageLength));
   dispatch(calculateMonthlyPayment());
 };
