ff8d23e616671f326dbf8919e6f710b8d4b7d3cc
seanmcquaid <sean.mcquaid2@verizon.net>
2021-08-05 22:10:50 -0400

    added in function for calculating monthly payment

--- src/utils/mortgageUtils.js
+++ src/utils/mortgageUtils.js
@@ -5,3 +5,17 @@
 // i = monthly interest rate
 
 // n = number of months required to repay the loan
+
+export const calculateMonthlyPayment = (
+  totalLoanAmount,
+  downPaymentAmount,
+  interestRate,
+  mortgageLength,
+) => {
+  const principal = totalLoanAmount - downPaymentAmount;
+  const months = mortgageLength * 12;
+  return (
+    (principal * Math.pow(1 + interestRate, months)) /
+    (Math.pow(1 + interestRate, months) - 1)
+  );
+};
