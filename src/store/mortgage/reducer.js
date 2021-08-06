6325332cfc3643a55a437ba4f101ed720594e527
seanmcquaid <sean.mcquaid2@verizon.net>
2021-08-05 22:54:28 -0400

    hooked up mortgage reducer for monthly payments

--- src/store/mortgage/reducer.js
+++ src/store/mortgage/reducer.js
@@ -1,8 +1,10 @@
+import { calculateMonthlyPayment } from '../../utils/mortgageUtils';
 import {
   SET_DOWN_PAYMENT,
   SET_INTEREST_RATE,
   SET_MORTGAGE_LENGTH,
   SET_TOTAL_MORTGAGE,
+  CALCULATE_MONTHLY_PAYMENT,
 } from './actionTypes';
 
 const initialState = {
@@ -36,6 +38,16 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         totalMortgage: action.payload.totalMortgage,
+      };
+    case CALCULATE_MONTHLY_PAYMENT:
+      return {
+        ...state,
+        monthlyPayment: calculateMonthlyPayment(
+          state.totalMortgage,
+          state.downPayment,
+          state.interestRate,
+          state.mortgageLength,
+        ),
       };
     default:
       return {
