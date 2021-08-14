2cf1a3cf7cd1dbadb02edb21d92ab9b6d0ca405f
seanmcquaid <sean.mcquaid2@verizon.net>
2021-08-14 15:15:57 -0400

    finished up mortgage info form

--- src/containers/Mortgage/MortgageInfoForm/index.js
+++ src/containers/Mortgage/MortgageInfoForm/index.js
@@ -5,16 +5,22 @@ import { Dropdown, TextInput } from '../../../components';
 import {
   mortgageLengthSelector,
   interestRateSelector,
+  totalMortgageSelector,
+  downPaymentSelector,
 } from '../../../store/mortgage/selectors';
 import {
   setMortgageLengthAction,
   setInterestRateAction,
+  setTotalMortgageAction,
+  setDownPaymentAction,
 } from '../../../store/mortgage/actions';
 
 const MortgageInfoForm = () => {
   const mortgageLength = useSelector(mortgageLengthSelector);
   const mortgageLengthOptions = useMemo(() => [15, 30], []);
   const interestRate = useSelector(interestRateSelector);
+  const totalMortgage = useSelector(totalMortgageSelector);
+  const downPayment = useSelector(downPaymentSelector);
   const dispatch = useDispatch();
 
   const mortgageLengthOnChange = (event) => {
@@ -23,6 +29,14 @@ const MortgageInfoForm = () => {
 
   const interestRateOnChange = (event) => {
     dispatch(setInterestRateAction(Number(event.target.value)));
+  };
+
+  const totalMortgageOnChange = (event) => {
+    dispatch(setTotalMortgageAction(Number(event.target.value)));
+  };
+
+  const downPaymentOnChange = (event) => {
+    dispatch(setDownPaymentAction(Number(event.target.value)));
   };
 
   return (
@@ -40,6 +54,20 @@ const MortgageInfoForm = () => {
         type="number"
         label="Interest Rate"
         name="interestRate"
+      />
+      <TextInput
+        onChange={totalMortgageOnChange}
+        value={totalMortgage}
+        type="number"
+        label="Total Mortgage"
+        name="totalMortgage"
+      />
+      <TextInput
+        onChange={downPaymentOnChange}
+        value={downPayment}
+        type="number"
+        label="Down Payment"
+        name="downPayment"
       />
     </Form>
   );
