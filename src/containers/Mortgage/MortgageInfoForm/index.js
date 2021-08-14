bc892dc685f81c7b4b403e2888bfc78907cbc3d1
seanmcquaid <sean.mcquaid2@verizon.net>
2021-08-14 15:03:52 -0400

    finished mortgage info form

--- src/containers/Mortgage/MortgageInfoForm/index.js
+++ src/containers/Mortgage/MortgageInfoForm/index.js
@@ -1,22 +1,46 @@
-import { useFormik } from 'formik';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { Dropdown, TextInput } from '../../../components';
+import {
+  mortgageLengthSelector,
+  interestRateSelector,
+} from '../../../store/mortgage/selectors';
+import {
+  setMortgageLengthAction,
+  setInterestRateAction,
+} from '../../../store/mortgage/actions';
 
 const MortgageInfoForm = () => {
+  const mortgageLength = useSelector(mortgageLengthSelector);
+  const mortgageLengthOptions = useMemo([15, 30], []);
+  const interestRate = useSelector(interestRateSelector);
   const dispatch = useDispatch();
-  // values are pulled in from selectors via redu
 
-  const onChange = (event) => {
-    // dispatch change to redux here for on change
-    dispatch();
+  const mortgageLengthOnChange = (event) => {
+    dispatch(setMortgageLengthAction(Number(event.target.value)));
+  };
+
+  const interestRateOnChange = (event) => {
+    dispatch(setInterestRateAction(event.target.value));
   };
 
   return (
     <Form>
-      <Dropdown onChange={onChange} value={state.mortgageLength} />
-      <TextInput onChange={onChange} value={state.interestRate} />
+      <Dropdown
+        onChange={mortgageLengthOnChange}
+        value={mortgageLength}
+        name="mortgageLength"
+        options={mortgageLengthOptions}
+        label="Mortgage Length"
+      />
+      <TextInput
+        onChange={interestRateOnChange}
+        value={interestRate}
+        type="number"
+        label="Interest Rate"
+        name="interestRate"
+      />
     </Form>
   );
 };
