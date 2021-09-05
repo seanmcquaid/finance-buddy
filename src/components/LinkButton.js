685930602907a05beb82243fbe5ad9da1447030b
seanmcquaid <sean.mcquaid2@verizon.net>
2021-09-05 15:44:46 -0400

    finished home page

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import propTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import constants from '../constants';
 
 const LinkButton = ({ label, to }) => (
   <StyledLinkButton to={to} data-testid={`${label}LinkButton`}>
@@ -8,11 +9,24 @@ const LinkButton = ({ label, to }) => (
   </StyledLinkButton>
 );
 
+const StyledLinkButton = styled(Link)`
+  padding: 1rem;
+  font-family: ${constants.paragraphFont};
+  border: ${(props) =>
+    props.secondary
+      ? `1px solid ${constants.black}`
+      : `1px solid ${constants.white}`};
+  color: ${(props) =>
+    props.secondary ? `${constants.black}` : `${constants.white}`};
+  background-color: ${(props) =>
+    props.secondary ? `${constants.skyBlue}` : `${constants.green}`};
+  border-radius: 16px;
+  outline: none;
+`;
+
 LinkButton.propTypes = {
   label: propTypes.string.isRequired,
   to: propTypes.string.isRequired,
 };
-
-const StyledLinkButton = styled(Link)``;
 
 export default LinkButton;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,16 +1,34 @@
 import styled from 'styled-components';
+import { H1, LinkButton, P } from '../../components';
 
 const Home = () => (
   <PageContainer>
-    <Header></Header>
-    <Main></Main>
+    <Header>
+      <H1>Welcome to Finance Buddy!</H1>
+    </Header>
+    <Main>
+      <P>Select your needed area of assistance down below!</P>
+      <LinksContainer>
+        <LinkButton label="Mortgage" to="/mortgage" />
+        <LinkButton label="Retirement" to="/retirement" />
+        <LinkButton label="Budge" to="/budget" />
+      </LinksContainer>
+    </Main>
   </PageContainer>
 );
 
-const PageContainer = styled.div``;
+const PageContainer = styled.div`
+  width: 100%;
+`;
 
 const Header = styled.header``;
 
-const Main = styled.main``;
+const Main = styled.main`
+  width: 100%;
+`;
+
+const LinksContainer = styled.div`
+  width: 100%;
+`;
 
 export default Home;
